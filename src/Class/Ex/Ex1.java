c2817b72bb80bbd7448ee83b7258b61aa288f353
cks-wls <slo0521@naver.com>
2026-08-21 16:39:19 +0900

    java-basic : Class

---
+++ src/Class/Ex/Ex1.java
@@ -0,0 +1,14 @@
+package Class.Ex;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        MovieReview movieReview1 = new MovieReview();
+        movieReview1.title = "타이타닉";
+        movieReview1.review = "굿굿";
+        MovieReview[] arr = {movieReview1};
+        for (MovieReview value : arr) {
+            System.out.println("제목 : " + value.title + " 리뷰 : " + value.review);
+        }
+
+    }
+}
